9f819e3d6c53674e14b1947c4eda4ad1dd1f3a2f
dlata29 <divya_9629@yahoo.com>
2025-09-15 06:38:41 +0530

    css to nav bar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="dvNavBar">
+        <h1 className="dvNavBarLeftHeading">Welcome</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
